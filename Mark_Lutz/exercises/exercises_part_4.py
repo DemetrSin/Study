114af716460e880c3265120e4c626c004936bc40
Demetr Sin <sonofgod1551@gmail.com>
2024-03-14 14:06:52 +0200

    processed exercises part 4

--- Mark_Lutz/exercises/exercises_part_4.py
+++ Mark_Lutz/exercises/exercises_part_4.py
@@ -1,4 +1,7 @@
 import timeit
+import time
+import math
+from functools import reduce
 
 
 # Exercise 1
@@ -183,3 +186,168 @@ print(f6(1))  # (1, 2, ())
 print(f6(1, 3, 4))  # (1, 3, (4,))
 
 
+# Exercise 8
+
+
+def prime(y):
+    if y <= 1:
+        return y, 'not prime'
+    else:
+        x = y // 2
+        while x > 1:
+            if y % x == 0:
+                return y, 'has factor', x
+            x -= 1
+        else:
+            return y, 'is prime'
+
+
+print(prime(13))  # (13, 'is prime')
+print(prime(13.0))  # (13.0, 'is prime')
+print(prime(15))  # (15, 'has factor', 5)
+print(prime(15.0))  # (15.0, 'has factor', 5.0)
+print(prime(101))  # (101, 'is prime')
+print(prime(-1))  # (-1, 'not prime')
+print(prime(0))  # (0, 'not prime')
+
+
+# Exercise 9
+from math import sqrt
+
+lst = [2, 4, 9, 16, 25]
+
+
+for x in range(len(lst)):
+    lst[x] = sqrt(lst[x])
+print(lst)  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+
+# OR
+lst = [2, 4, 9, 16, 25]
+res = lst[:]
+for x in range(len(lst)):
+    res[x] = sqrt(res[x])
+print(lst)  # [2, 4, 9, 16, 25]
+print(res)  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+
+# OR
+res = []
+for x in lst:
+    res.append(sqrt(x))
+print(res)  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+
+lst = [2, 4, 9, 16, 25]
+print(list(map(lambda x: sqrt(x), lst)))  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+print([sqrt(x) for x in lst])  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+print(list(sqrt(x) for x in lst))  # [1.4142135623730951, 2.0, 3.0, 4.0, 5.0]
+
+
+# Exercise 10
+
+
+def custom_timer(runs=1000000):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            total_time = 0
+            for _ in range(runs):
+                start = time.perf_counter()
+                result = func(*args, **kwargs)
+                end = time.perf_counter()
+                total_time += end - start
+            avg_time = total_time / runs
+            return f"{func.__name__} >> {total_time} >> {avg_time}"
+        return wrapper
+    return decorator
+
+
+@custom_timer()
+def my_pow(x):
+    return pow(x, 0.5)
+
+
+print(my_pow(200000000000000000000000000000000000000000))
+# 4.472135954999579e+20 >>> my_pow >> 0.19528969996827072 >> 1.9528969996827072e-07
+
+
+@custom_timer()
+def use_sqrt(x):
+    return sqrt(x)
+
+
+print(use_sqrt(200000000000000000000000000000000000000000))
+# 4.472135954999579e+20 >>> use_sqrt >> 0.17259750131051987 >> 1.7259750131051988e-07
+
+
+@custom_timer()
+def standard_sqrt(x):
+    return x ** 0.5
+
+
+print(standard_sqrt(200000000000000000000000000000000000000000))
+# 4.472135954999579e+20 >>> standard_sqrt >> 0.18260599867790006 >> 1.8260599867790006e-07
+
+
+@custom_timer()
+def lst_comp():
+    return [x ** 2 for x in range(100)]
+
+
+@custom_timer()
+def dict_comp():
+    return {k: k ** 2 for k in range(100)}
+
+
+print(lst_comp())  # lst_comp >> 2.6080221015872667 >> 2.608022101587267e-06
+print(dict_comp())  # dict_comp >> 3.41428259958775 >> 3.41428259958775e-06
+
+
+# Exercise 11
+
+def count_down(x):
+    if x == 0:
+        return 'stop'
+    else:
+        return x, count_down(x-1)
+
+
+print(count_down(5))  # (5, (4, (3, (2, (1, 'stop')))))
+
+
+def count_down2(x):
+    yield from range(x, 0, -1)
+
+
+print(list(count_down(5)))  # [5, (4, (3, (2, (1, 'stop'))))]
+
+
+# Exercise 12
+
+@custom_timer()
+def factorial(z):
+    n = 1
+    for x in range(z, 0, -1):
+        n *= x
+    return n
+
+
+@custom_timer()
+def factorial_recursion(z):
+    if z == 0 or z == 1:
+        return 1
+    else:
+        return z * factorial_recursion(z - 1)
+
+
+@custom_timer()
+def factorial_reduce(z):
+    return reduce(lambda x, y: x * y, range(1, z + 1))
+
+
+@custom_timer()
+def math_factorial(z):
+    return math.factorial(z)
+
+
+print(factorial(6))  # factorial >> 0.2686018012500426 >> 2.686018012500426e-07
+# print(factorial_recursion(6))
+print(factorial_reduce(6))  # factorial_reduce >> 0.3641667003648763 >> 3.641667003648763e-07
+print(math_factorial(6))  # math_factorial >> 0.12732160080850008 >> 1.2732160080850008e-07
