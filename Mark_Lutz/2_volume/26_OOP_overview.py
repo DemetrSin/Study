833b35121cfc07b183883fb372fb7f2a30e26a35
Demetr Sin <sonofgod1551@gmail.com>
2024-03-15 10:15:01 +0200

    processed 26h chapter

---
+++ Mark_Lutz/2_volume/26_OOP_overview.py
@@ -0,0 +1,39 @@
+class C1:
+    def __init__(self, who):
+        self.name = who
+
+    def set_default(self):
+        self.name = 'Default'
+
+
+class C2(C1):
+    def set_default(self):
+        self.name = 'Own version'
+
+
+class C(C1):
+    pass
+
+
+i = C('Demetr')
+i1 = C1('Nick')
+i2 = C2('Alina')
+
+
+print(i1)  # <__main__.C object at 0x000001D952542D10>
+print(C2)  # <class '__main__.C2'>
+print(C)  # <class '__main__.C'>
+print(i.name)  # Demetr
+print(i1.name)  # Nick
+print(i2.name)  # Alina
+
+
+def defaulter(lst):
+    final_data = []
+    for x in lst:
+        x.set_default()
+        final_data.append(x)
+    return [x.name for x in final_data]
+
+
+print(defaulter([i, i1, i2]))  # ['Default', 'Default', 'Default']
