2959a66e27ab27b79ad174634495d211a77b95ad
Demetr Sin <sonofgod1551@gmail.com>
2024-03-18 07:47:38 +0200

    processed 27h chapter

--- Mark_Lutz/2_volume/27_OOP_basics.py
+++ Mark_Lutz/2_volume/27_OOP_basics.py
@@ -72,4 +72,52 @@ a.mul(3)
 print(a)  # ThirdClass : abcabcabc
 
 
+class Pass:
+    pass
 
+
+Pass.name = 'pass'
+Pass.do = None
+
+
+pass_instance = Pass()
+pass_instance2 = Pass()
+print(pass_instance.name)  # pass
+print(pass_instance.do)  # None
+
+pass_instance.name = 'New name'
+print(Pass.name, pass_instance.name, pass_instance2.name)  # pass New name pass
+print([x for x in Pass.__dict__ if not x.startswith('_')])  # ['name', 'do']
+print([x for x in pass_instance.__dict__ if not x.startswith('_')])  # ['name']
+print([x for x in pass_instance2.__dict__ if not x.startswith('_')])  # []
+print(pass_instance2.__class__)  # <class '__main__.Pass'>
+
+print(Pass.__bases__)  # (<class 'object'>,)
+print(ThirdClass.__bases__)  # (<class '__main__.SecondCLass'>,)
+
+
+def upper_name(obj):
+    return obj.name.upper()
+
+
+print(upper_name(pass_instance2))  # PASS
+
+
+Pass.method = upper_name
+print(Pass.method(pass_instance))  # NEW NAME
+print(pass_instance2.method())  # PASS
+
+
+class Person:
+    def __init__(self, name, jobs, age=None):
+        self.name = name
+        self.jobs = jobs
+        self.age = age
+
+    def info(self):
+        return (self.name, self.jobs, self.age) if self.age else (self.name, self.jobs)
+
+
+person1 = Person('Demetr', 'Developer', 26)
+person2 = Person('Alina', 'HR')
+print(person1.info())
