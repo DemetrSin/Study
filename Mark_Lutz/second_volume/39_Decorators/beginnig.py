4d5a0d8162323dea8c89e9bacb633440386cd391
Demetr Sin <sonofgod1551@gmail.com>
2024-04-01 08:03:20 +0300

    processing 39h chapter

--- Mark_Lutz/second_volume/39_Decorators/beginnig.py
+++ Mark_Lutz/second_volume/39_Decorators/beginnig.py
@@ -15,7 +15,7 @@ class C:
 
 
 x = C(6, 7)
-print(x.attr)
+print(x.attr)  # spam
 
 
 # wrong example
@@ -40,8 +40,8 @@ class C:
 
 x = C(5)
 y = C(5)
-print(x.attr)
-print(y.attr)
+print(x.attr)  # spam
+print(y.attr)  # spam
 
 
 def d1(f):
@@ -64,3 +64,133 @@ def func():
 
 
 print(func())  # xyzspam
+
+
+class Tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args):
+        self.calls += 1
+        print(f"call {self.calls} to {self.func.__name__}")
+        self.func(*args)
+
+
+@Tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+spam(1, 2, 3)  # call 1 to spam  6
+spam('a', 'b', 'c')  # call 2 to spam abc
+print(spam.calls)  # 2
+print(spam)  # <__main__.Tracer object at 0x0000019A07D93610>
+
+
+# the same without decorators
+
+calls = 0
+
+
+def tracer(func, *args):
+    global calls
+    calls += 1
+    print(f"call {calls} to {func.__name__}")
+    func(*args)
+
+
+def spam(a, b, c):
+    print(a + b + c)
+
+
+tracer(spam, 1, 2, 3)  # call 1 to spam  6
+tracer(spam, 'a', 'b', 'c')  # call 2 to spam abc
+print(calls)  # 2
+print(spam)  # <function spam at 0x000001F856856340>
+
+
+# with kwargs
+
+class Tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        print(f"call {self.calls} to {self.func.__name__}")
+        self.func(*args, **kwargs)
+
+
+@Tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+@Tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+spam(a=4, b=5, c=6)   # call 1 to spam  15
+eggs(2, 16)  # call 1 to eggs  65536
+eggs(4, y=4)  # call 2 to eggs  256
+
+
+# func option
+
+
+def tracer(func):
+    calls = 0
+
+    def wrapper(*args, **kwargs):
+        nonlocal calls
+        calls += 1
+        print(f"call {calls} to {func.__name__}")
+        func(*args, **kwargs)
+
+    return wrapper
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+@tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+print('\n'*5)
+spam(a=4, b=5, c=6)   # call 1 to spam  15
+eggs(2, 16)  # call 1 to eggs  65536
+eggs(4, y=4)  # call 2 to eggs  256
+
+
+# or with func attrs
+
+def tracer(func):
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        print(f"call {wrapper.calls} to {func.__name__}")
+        func(*args, **kwargs)
+    wrapper.calls = 0
+    return wrapper
+
+
+@tracer
+def spam(a, b, c):
+    print(a + b + c)
+
+
+@tracer
+def eggs(x, y):
+    print(x ** y)
+
+
+print('\n'*5)
+spam(a=4, b=5, c=6)   # call 1 to spam  15
+eggs(2, 16)  # call 1 to eggs  65536
+eggs(4, y=4)  # call 2 to eggs  256
